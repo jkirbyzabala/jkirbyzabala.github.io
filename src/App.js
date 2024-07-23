1413686e2d04f29c4a3c14a240d8d4dfecd18cd7
Kirby Zabala <jkirbyzabala@gmail.com>
2024-07-22 21:49:45 -0600

    updated the font

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './components/Header';
 import TypeButtons from './components/TypeButtons';
-import PokemonList from '.components/PokemonList';
+import PokemonList from './components/PokemonList';
 import GenerationFilter from './components/GenerationFilter';
 import './App.css';
 
