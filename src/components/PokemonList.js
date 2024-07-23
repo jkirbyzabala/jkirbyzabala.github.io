13a34bb204fe3c7e595a0717a961a9484c459a95
Kirby Zabala <jkirbyzabala@gmail.com>
2024-07-22 21:56:21 -0600

    fix the Pokemon List

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import './PokemonList.css';
 
-function PokemonList({ pokemonList, addToTeam }) {
+// PokemonList component to display available Pokémon of the selected type
+const PokemonList = ({ pokemonList, onAddToTeam }) => {
   return (
-    <div>
-      <h2>Available Pokémon</h2>
-      <ul>
-        {pokemonList.map((pokemon, index) => (
-          <li key={index}>
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-            {pokemon.name}
-            <button onClick={() => addToTeam(pokemon)}>Add to Team</button>
-          </li>
-        ))}
-      </ul>
+    <div className="pokemon-list">
+      {pokemonList.length === 0 ? (
+        <p>Please select a Pokémon type to view available Pokémon.</p>
+      ) : (
+        <ul>
+          {pokemonList.map((pokemon, index) => (
+            <li key={index}>
+              {pokemon.name}
+              <button onClick={() => onAddToTeam(pokemon)}>Add to Team</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default PokemonList;
